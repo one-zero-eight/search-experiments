719ea3fa2d798f80c2606891c8327cad60ae5e0c
Ruslan Bel'kov <ruslan.belckov@yandex.ru>
2024-06-21 01:10:17 +0300

    chore: cleanup

--- pymupdf4llm/_pymupdf.py
+++
@@ -1,9 +0,0 @@
-__all__ = ["pymupdf"]
-
-try:
-    import pymupdf  # available with v1.24.3
-except ImportError:
-    import fitz as pymupdf
-
-if pymupdf.pymupdf_version_tuple < (1, 24, 2):
-    raise NotImplementedError("PyMuPDF version 1.24.2 or later is needed.")
